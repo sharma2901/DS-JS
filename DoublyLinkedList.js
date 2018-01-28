3e504611ca9af7750b1323d427c487a8472bf398
Surya Sharma <suryakant@ideaplunge.com>
2018-01-28 07:31:47 +0530

    Doubly Linked List

--- DoublyLinkedList.js
+++ DoublyLinkedList.js
@@ -1,5 +1,5 @@
 // Constructor function for Linked List, head and tail will contain Object of Node
-function LinkedList() {
+function DoublyLinkedList() {
   this.head = null;
   this.tail = null;
 }
@@ -11,24 +11,24 @@ function Node(value, next, prev) {
   this.prev = prev;
 }
 
-// Adds value to the head of LinkedList
-LinkedList.prototype.addToHead = function(value) {
+// Adds value to the head of DoublyLinkedList
+DoublyLinkedList.prototype.addToHead = function(value) {
   let newNode = new Node(value, this.head, null);
   if (this.head) this.head.prev = newNode;
   else this.tail = newNode;
   this.head = newNode;
 };
 
-// Adds value to the tail of LinkedList
-LinkedList.prototype.addToTail = function(value) {
+// Adds value to the tail of DoublyLinkedList
+DoublyLinkedList.prototype.addToTail = function(value) {
   let newNode = new Node(value, null, this.tail);
   if (this.tail) this.tail.next = newNode;
   else this.head = newNode;
   this.tail = newNode;
 };
 
-// Removes the head from LinkedList, sets head to next node, and returns the value of old head
-LinkedList.prototype.removeHead = function() {
+// Removes the head from DoublyLinkedList, sets head to next node, and returns the value of old head
+DoublyLinkedList.prototype.removeHead = function() {
   if (!this.head) return null;
   let value = this.head.value;
   this.head = this.head.next;
@@ -37,8 +37,8 @@ LinkedList.prototype.removeHead = function() {
   return value;
 };
 
-// Removes the tail from LinkedList, sets tail to prev node, and returns the value of old tail
-LinkedList.prototype.removeTail = function() {
+// Removes the tail from DoublyLinkedList, sets tail to prev node, and returns the value of old tail
+DoublyLinkedList.prototype.removeTail = function() {
   if (!this.tail) return null;
   let value = this.tail.value;
   this.tail = this.tail.prev;
@@ -48,7 +48,7 @@ LinkedList.prototype.removeTail = function() {
 };
 
 // returns the value if found, null otherwise
-LinkedList.prototype.search = function(value) {
+DoublyLinkedList.prototype.search = function(value) {
   if (!this.head) return null;
   let currNode = this.head;
   while (currNode) {
@@ -59,7 +59,7 @@ LinkedList.prototype.search = function(value) {
 };
 
 // returns index of the value if found, -1 otherwise
-LinkedList.prototype.indexOf = function(value) {
+DoublyLinkedList.prototype.indexOf = function(value) {
   if (!this.head) return -1;
   let currNode = this.head;
   let counter = 0;
@@ -72,12 +72,12 @@ LinkedList.prototype.indexOf = function(value) {
 };
 
 //Examples
-let ll = new LinkedList();
+let ll = new DoublyLinkedList();
 ll.addToHead(20); // Add 20 to Head
 ll.addToHead(10); // Add 10 to Head
 ll.addToTail(30); // Add 30 to Tail
 ll.addToTail(40); // Add 40 to Tail
-console.log(ll); //Prints complete LinkedList
+console.log(ll); //Prints complete DoublyLinkedList
 console.log(ll.removeHead()); // Removes Head and returns 10;
 console.log(ll.removeTail()); // Removes Tail and returns 40;
 console.log(ll.search(20)); // Returns 20
